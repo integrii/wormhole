ac742251949d2f5dbb0621e8a4361ba134c91009
Eric Greer <integrii@gmail.com>
2016-12-31 04:56:35 +0800

    updated readme with icon. gitignore file.  adding flags for in and out params

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # wormhole
-Makes a wormhole between one TCP socket to another transparently.
+
+![Wormhole Icon](https://raw.githubusercontent.com/integrii/wormhole/master/icon.png)
+
+Very simple [go](https://golang.org) program that transparently redirects TCP connections from one TCP socket to another.
+
+## installation
+`go get -u github.com/integrii/wormhole`
+
+## usage
+
+Just execute the binary from your terminal with a `-?` flag to see help that looks like this:
+
+```
+./wormhole -?
+flag provided but not defined: -?
+Usage of ./wormhole:
+  -from string
+    	The address and port that wormhole should listen on.  Connections enter here. (default "0.0.0.0:443")
+  -to string
+    	Specifies the address and port that wormhole should redirect TCP connections to.  Connections exit here. (default "127.0.0.1:80")
+```
+
+
+### example
+
+`./wormhole -from 0.0.0.0:22 8.8.8.8:2222`
--- main.go
+++ main.go
@@ -1,43 +1,57 @@
 package main
 
 import (
+	"flag"
 	"io"
 	"log"
 	"net"
 )
 
+var from *string
+var to *string
+
+func init() {
+	from = flag.String("from", "0.0.0.0:443", "The address and port that wormhole should listen on.  Connections enter here.")
+	to = flag.String("to", "127.0.0.1:80", "Specifies the address and port that wormhole should redirect TCP connections to.  Connections exit here.")
+	flag.Parse()
+}
+
 func main() {
-	// Listen on TCP port 443 on all interfaces.
-	l, err := net.Listen("tcp", ":443")
+
+	// Listen on the specified TCP port on all interfaces.
+	l, err := net.Listen("tcp", *from)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer l.Close()
 	for {
 		// Wait for a connection.
-		conn, err := l.Accept()
+		c, err := l.Accept()
 		if err != nil {
 			log.Fatal(err)
 		}
-		// Handle the connection in a new goroutine.
-		// The loop then returns to accepting, so that
-		// multiple connections may be served concurrently.
-		go func(c net.Conn) {
-			defer c.Close()
-			log.Println("Opening wormhole")
 
-			// connect to the destination tcp port
-			destConn, err := net.Dial("tcp", "localhost:22")
-			if err != nil {
-				log.Fatal("Error connecting to destination port")
-			}
-			defer destConn.Close()
-			log.Println("Wormhole open")
-
-			go func() { io.Copy(c, destConn) }()
-			io.Copy(destConn, c)
-
-			log.Println("Stopping wormhole")
-		}(conn)
+		// handle the connection in a goroutine
+		go wormhole(c)
 	}
+}
+
+// wormhole opens a wormhole from the client connection
+// to user the specified destination
+func wormhole(c net.Conn) {
+	defer c.Close()
+	log.Println("Opening wormhole from", c.RemoteAddr())
+
+	// connect to the destination tcp port
+	destConn, err := net.Dial("tcp", *to)
+	if err != nil {
+		log.Fatal("Error connecting to destination port")
+	}
+	defer destConn.Close()
+	log.Println("Wormhole open from", c.RemoteAddr())
+
+	go func() { io.Copy(c, destConn) }()
+	io.Copy(destConn, c)
+
+	log.Println("Stopping wormhole from", c.RemoteAddr())
 }
